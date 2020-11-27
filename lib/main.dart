f881712553e4d5afc310b87cbefcdbf201d9d6a7
sneha <snehasaj1999@gmail.com>
2020-11-27 16:09:14 +0530

    Added Intro (folder) and Introduction screens (only UI)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carbonfootprint/Sample Page/samplePage.dart';
+import 'package:carbonfootprint/Intro/intro.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: OnBoardingPage(),
     );
   }
 }
