e63e6e98074b49c24dff463ff679c0fba1631057
Megha K C <39724388+Megha-KC@users.noreply.github.com>
2020-12-03 18:25:19 +0530

    Merging conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:carbonfootprint/Navigation/navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:carbonfootprint/Login/login.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Carbon Footprint Tracker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: SignInDemo(),
-    );
+        title: 'Carbon Footprint Tracker',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: NavigationBar() // SignInDemo(),
+        );
   }
 }
