d889352cb6f300bcee5c361bbb689e7acbd94544
Steev James <steev.james11@gmail.com>
2021-02-04 16:43:37 +0530

    Update intro.dart

--- lib/Intro/intro.dart
+++ lib/Intro/intro.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:carbonfootprint/Login/login.dart';
-import 'package:carbonfootprint/Homepage/homepage.dart';
-import 'package:carbonfootprint/Components/styling.dart';
 
 class OnBoardingPage extends StatefulWidget {
   @override
@@ -28,12 +26,18 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
   @override
   Widget build(BuildContext context) {
     const bodyStyle = TextStyle(fontSize: 19.0);
-    const pageDecoration = const PageDecoration(
-      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
-      bodyTextStyle: bodyStyle,
-      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-      pageColor: Colors.white,
-      imagePadding: EdgeInsets.zero,
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(
+        fontWeight: FontWeight.w700,
+        color: Color(0xff555555),
+        fontSize: 25.0,
+        // fontStyle: FontStyle.italic,
+      ),
+      bodyTextStyle: TextStyle(
+        fontSize: 18.0,
+        color: Colors.grey,
+        fontStyle: FontStyle.italic,
+      ),
     );
 
     return IntroductionScreen(
@@ -44,125 +48,44 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
           body:
               "A carbon footprint is the amount of greenhouse gases—primarily carbon dioxide—released into the atmosphere by a particular human activity. ",
           image: Center(
-              child: Image.asset("assets/images/global.jpg", height: 250.0)),
-
-          //image: _buildImage('assets/images/back'),
-          decoration: const PageDecoration(
-            titleTextStyle: TextStyle(
-                fontWeight: FontWeight.w700,
-                color: Colors.black54,
-                fontSize: 30.0,
-                fontStyle: FontStyle.italic),
-            bodyTextStyle: TextStyle(
-                fontSize: 20.0,
-                color: Colors.grey,
-                fontStyle: FontStyle.italic),
+            child: Image.asset("assets/images/global.jpg", height: 200.0),
           ),
+          //image: _buildImage('assets/images/back'),
+          decoration: pageDecoration,
         ),
         PageViewModel(
           title: "The 3 R's",
           body:
               "Reduce, Reuse, Recycle – these three 'R' words are an important part of sustainable living, as they help to cut down on the amount of waste we have to throw away.",
           image: Center(
-              child: Image.asset("assets/images/planet5.jpg", height: 285.0)),
-          decoration: const PageDecoration(
-            titleTextStyle: TextStyle(
-                fontWeight: FontWeight.w700,
-                color: Colors.black54,
-                fontSize: 30.0,
-                fontStyle: FontStyle.italic),
-            bodyTextStyle: TextStyle(
-                fontSize: 20.0,
-                color: Colors.grey,
-                fontStyle: FontStyle.italic),
-          ),
+              child: Image.asset("assets/images/planet5.jpg", height: 200.0)),
+          decoration: pageDecoration,
         ),
         PageViewModel(
           title: "Reduce",
-          body: "Reduce the amount of materials and goods you consume.",
+          body:
+              "Reducing what is produced and what is consumed is essential to the best hierarchy. The logic behind it is simple to understand – if there is less waste, then there is less to recycle and reuse.",
           image: Center(
-              child: Image.asset("assets/images/planet1.jpg", height: 250.0)),
-          decoration: const PageDecoration(
-            titleTextStyle: TextStyle(
-                fontWeight: FontWeight.w700,
-                color: Colors.black54,
-                fontSize: 30.0,
-                fontStyle: FontStyle.italic),
-            bodyTextStyle: TextStyle(
-                fontSize: 20.0,
-                color: Colors.grey,
-                fontStyle: FontStyle.italic),
-          ),
+              child: Image.asset("assets/images/planet1.jpg", height: 200.0)),
+          decoration: pageDecoration,
         ),
         PageViewModel(
           title: "Reuse",
           body:
               "Reusing is the act of taking old items that you might consider throwing away and finding a new use for them.",
           image: Center(
-              child: Image.asset("assets/images/planet3.jpg", height: 250.0)),
-          decoration: const PageDecoration(
-            titleTextStyle: TextStyle(
-                fontWeight: FontWeight.w700,
-                color: Colors.black54,
-                fontSize: 30.0,
-                fontStyle: FontStyle.italic),
-            bodyTextStyle: TextStyle(
-                fontSize: 20.0,
-                color: Colors.grey,
-                fontStyle: FontStyle.italic),
-          ),
+              child: Image.asset("assets/images/planet3.jpg", height: 200.0)),
+          decoration: pageDecoration,
         ),
         PageViewModel(
           title: "Recycle",
           body:
               "This is probably the most well-known and well-understood of the 3 Rs. If you have recycling pick-up services, you will be familiar with recycling paper, plastic and metal materials.",
           image: Center(
-              child: Image.asset("assets/images/planet4.jpg", height: 250.0)),
-          decoration: const PageDecoration(
-            titleTextStyle: TextStyle(
-                fontWeight: FontWeight.w700,
-                color: Colors.black54,
-                fontSize: 30.0,
-                fontStyle: FontStyle.italic),
-            bodyTextStyle: TextStyle(
-                fontSize: 20.0,
-                color: Colors.grey,
-                fontStyle: FontStyle.italic),
+            child: Image.asset("assets/images/planet4.jpg", height: 200.0),
           ),
+          decoration: pageDecoration,
         ),
-//        PageViewModel(
-//          title: "Reuse",
-//          body:
-//              "Reusing is the act of taking old items that you might consider throwing away and finding a new use for them.",
-//          image: _buildImage(''),
-//          footer: RaisedButton(
-//            onPressed: () {
-//              introKey.currentState?.animateScroll(0);
-//            },
-//            child: const Text(
-//              'FooButton',
-//              style: TextStyle(color: Colors.white),
-//            ),
-//            color: Colors.lightBlue,
-//            shape: RoundedRectangleBorder(
-//              borderRadius: BorderRadius.circular(8.0),
-//            ),
-//          ),
-//          decoration: pageDecoration,
-//        ),
-//        PageViewModel(
-//          title: "Recycle",
-//          bodyWidget: Row(
-//            mainAxisAlignment: MainAxisAlignment.center,
-//            children: const [
-//              Text("Click on ", style: bodyStyle),
-//              Icon(Icons.edit),
-//              Text(" to edit a post", style: bodyStyle),
-//            ],
-//          ),
-//          image: _buildImage('img1'),
-//          decoration: pageDecoration,
-//        ),
       ],
       onDone: () => _onIntroEnd(context),
       //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
@@ -171,15 +94,19 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
       nextFlex: 0,
       skip: const Text('Skip',
           style: TextStyle(
-            color: Colors.grey,
+            color: Color(0xff004c48),
           )),
-      next: const Icon(Icons.arrow_forward, color: Colors.grey),
+      next: const Icon(
+        Icons.arrow_forward,
+        color: Color(0xff004c48),
+      ),
       done: const Text('Done',
           style: TextStyle(
             fontWeight: FontWeight.w600,
           )),
       dotsDecorator: const DotsDecorator(
         size: Size(10.0, 10.0),
+        activeColor: Color(0xff004c48),
         color: Color(0xFFBDBDBD),
         activeSize: Size(22.0, 10.0),
         activeShape: RoundedRectangleBorder(
